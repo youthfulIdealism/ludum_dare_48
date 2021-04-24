37265fa46f2d54dd3d8d33399b2a5322316dc059
youthfulIdealism <samuel@dennisjrichardson.com>
2021-04-24 09:27:56 -0700

    added step sounds

--- src/behaviors/behavior_immune_time.js
+++ src/behaviors/behavior_immune_time.js
@@ -4,5 +4,4 @@ import * as Victor from 'victor'
 let behavior_immune_time = new Behavior('immune_time', (entity, sim_space, parameters, memory, context) => {
     if (entity.memory.immune_time === undefined || Number.isNaN(entity.memory.immune_time)) { entity.memory.immune_time = 0; }
     entity.memory.immune_time -= context.tpf;
-    console.log(entity.memory.immune_time - context.tpf)
 }, {});
--- src/behaviors/behavior_wasd.js
+++ src/behaviors/behavior_wasd.js
@@ -1,9 +1,19 @@
 import { Entity, SimSpace, Behavior } from 'yi-js-engine'
 import * as Victor from 'victor'
 
+const step_sound_time = 4;
+const step_sound_variance = 1;
+let sounds_by_size = ['./assets/sounds/player_0_step.wav',
+    './assets/sounds/player_1_step.wav',
+    './assets/sounds/player_2_step.wav',
+    './assets/sounds/player_3_step.wav',]
+
 let behavior_wasd = new Behavior('wasd', (entity, sim_space, parameters, memory, context) => {
     if (!sim_space.input_manager) { return; }
     if (!parameters.speed) { parameters.speed = 1; }
+    if (!parameters.time_until_step_sound) { parameters.time_until_step_sound = 0; }
+    if (parameters.last_size === undefined || parameters.last_size !== entity.memory.size) { parameters.time_until_step_sound = 0;}
+    parameters.last_size = entity.memory.size;
 
     let tpf = context.tpf;
     let direction = Victor(0, 0);
@@ -16,8 +26,19 @@ let behavior_wasd = new Behavior('wasd', (entity, sim_space, parameters, memory,
     if (input_manager.is_key_down('a')) { is_moving = true; direction.add(Victor(-1, 0)); }
     if (input_manager.is_key_down('d')) { is_moving = true; direction.add(Victor(1, 0)); }
 
+
+    
     if (is_moving) {
         entity.location.add(direction.normalize().multiply(Victor(tpf, tpf)).multiply(Victor(parameters.speed, parameters.speed)));
+
+        parameters.time_until_step_sound -= tpf;
+        if (parameters.time_until_step_sound <= 0) {
+            parameters.time_until_step_sound = step_sound_time + Math.random() * step_sound_variance;
+            let sound = sim_space.asset_manager.get_sound(sounds_by_size[entity.memory.size]);
+            sound.play();
+        }
+    } else {
+        parameters.time_until_step_sound = 0;
     }
     return false;
 
